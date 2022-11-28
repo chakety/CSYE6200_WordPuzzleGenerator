95009cfa4ca04dea2eaa9c1ed6d62af82f95dfca
Wenyi (Chakety) <64045577+chakety@users.noreply.github.com>
2022-11-28 15:16:02 -0500

    Update getVbox.java

--- wordsearch/getVbox.java
+++ wordsearch/getVbox.java
@@ -1,8 +1,17 @@
-public VBox getVbox(TextField text, Label l1) {
+package application;
+
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
+public class GetVbox {
+	public static VBox getVbox(TextField text, Label l1) {
 		VBox vbox = new VBox();
 		vbox.setAlignment(Pos.TOP_RIGHT);
 		vbox.getChildren().addAll(text,l1);
 		
 		return vbox;
 	}
-	
+
+}
